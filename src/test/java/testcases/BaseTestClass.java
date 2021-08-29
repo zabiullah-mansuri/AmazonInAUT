9935ba569dca6365fd071b969020c9a49c3e986d
“zabiullah92” <zabiullah92@gmail.com>
2021-08-29 15:12:20 +0530

    Added log4j properties file. Implemented in Base Test Class.

--- src/test/java/testcases/BaseTestClass.java
+++ src/test/java/testcases/BaseTestClass.java
@@ -1,6 +1,8 @@
 package testcases;
 
 import org.testng.annotations.BeforeClass;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -11,12 +13,16 @@ public class BaseTestClass {
 	String appUrl = "http://demo.guru99.com/V1/index.php";
 	String userId = "mngr350124";
 	String password = "udejugy";
+	Logger log4jlogger;
 
 	@BeforeClass
 	public void setup() {
 		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get(appUrl);
+
+		log4jlogger = Logger.getLogger("Amazon.in - AUT");
+		PropertyConfigurator.configure("log4j.properties");
 	}
 
 	@AfterClass
--- src/test/java/testcases/TC_001_LoginTest.java
+++ src/test/java/testcases/TC_001_LoginTest.java
@@ -9,12 +9,18 @@ public class TC_001_LoginTest extends BaseTestClass {
 	@Test
 	public void f() {
 		LoginPage loginPage = new LoginPage(driver);
+		log4jlogger.info("URL is opened.");
 		loginPage.setUserID(userId);
+		log4jlogger.info("Entered UserId : " + userId);
 		loginPage.setPassword(password);
+		log4jlogger.info("Entered Password : " + password);
 		loginPage.clickLogin();
+		log4jlogger.info("Clicked Login button.");
 		if (driver.getTitle().equals("GTPL Bank Manager HomePage")) {
+			log4jlogger.info("Login successful.");
 			Assert.assertTrue(true);
 		} else {
+			log4jlogger.info("Login failed.");
 			Assert.assertTrue(false);
 		}
 	}
